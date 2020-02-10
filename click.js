6a571ec157e37f8c6564058cd71d20da3b8b9577
Mafrans <malte.kluft@gmail.com>
2020-02-10 12:36:10 +0100

    Encase click script in its own scope

--- click.js
+++ click.js
@@ -1,31 +1,36 @@
-var clicks = 0;
-var clickssec = 0;
-var clickssec100 = 0;
-function onClick() {
-        clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-};
-
-function clickspsec(){
-    if (clicks > clickssec && clicks >= 100){ 
-        clicks = clicks - 100;
-        clickssec = clickssec + 10;
+// Encase the script in it's own scope, 
+// this makes sure other scripts do not override 
+// variables and that functions are exclusive to this script only
+(() => {
+    var clicks = 0;
+    var clickssec = 0;
+    var clickssec100 = 0;
+    function onClick() {
+            clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+    
+    function clickspsec(){
+        if (clicks > clickssec && clicks >= 100){ 
+            clicks = clicks - 100;
+            clickssec = clickssec + 10;
+        }
+        document.getElementById("clickssec").innerHTML = clickssec;
     }
-    document.getElementById("clickssec").innerHTML = clickssec;
-}
-
-function clickspsec100() {
-    if (clicks > clickssec100 && clicks >= 1000){
-        clicks = clicks - 1000;
-        clickssec100 = clickssec100 + 100
-        clickssec = clickssec + 100
+    
+    function clickspsec100() {
+        if (clicks > clickssec100 && clicks >= 1000){
+            clicks = clicks - 1000;
+            clickssec100 = clickssec100 + 100
+            clickssec = clickssec + 100
+        }
+        document.getElementById("clickssec").innerHTML = clickssec;
     }
-    document.getElementById("clickssec").innerHTML = clickssec;
-}
-
-var intervalID = window.setInterval(myCallback, 1000);
-function myCallback() {
-    clicks = clicks + clickssec;
-    document.getElementById("clicks").innerHTML = clicks;
-}
+    
+    var intervalID = window.setInterval(myCallback, 1000);
+    function myCallback() {
+        clicks = clicks + clickssec;
+        document.getElementById("clicks").innerHTML = clicks;
+    }
+})();
 
