a11708a4dd6902168cf70380bb1e1fb7dbf94d78
Mafrans <malte.kluft@gmail.com>
2020-02-10 13:03:58 +0100

    Update buttons to a more expandable system.

--- click.js
+++ click.js
@@ -6,12 +6,13 @@
     var clickssec = 0;
     var clickssec100 = 0;
     
-    // Saving the elements as constant variables
+    // Saving the elements as constant variables (variables that cannot be changed)
     // means that we do not have to query the DOM
     // every time the user clicks, greatly increasing performance
     const clickLabel = document.getElementById("clicks");
     const cpsLabel = document.getElementById("clickssec");
     const clickButton = document.getElementById("clickBtn");
+    const upgradeButtons = document.querySelectorAll("upgrade-btn");
 
     // By using both the 'click' and the 'ontouchstart' events
     // we can easily allow for touch support without having
@@ -34,22 +35,39 @@
         })
     });
     
-    function clickspsec(){
-        if (clicks > clickssec && clicks >= 100){ 
-            clicks = clicks - 100;
-            clickssec = clickssec + 10;
-        }
-        cpsLabel.innerHTML = clickssec;
-    }
-    
-    function clickspsec100() {
-        if (clicks > clickssec100 && clicks >= 1000){
-            clicks = clicks - 1000;
-            clickssec100 = clickssec100 + 100
-            clickssec = clickssec + 100
-        }
-        document.getElementById("clickssec").innerHTML = clickssec;
-    }
+
+    // Loop over every upgrade button asynchronously, 
+    // saving them as an 'element' paremeter.
+    //
+    // This means that rather than looping then doing 
+    // whatever comes next, we allow the program to continue
+    // doing tasks while we are looping.
+    //
+    // While this makes the loop hard to manage - for 
+    // example, we are no longer able to know the output
+    // of the loop unless we keep track of how many times it
+    // has looped and compare to an expected value.
+    upgradeButtons.forEach((element) => {
+
+        // Get the 'cost' and 'clicks' attributes from the element
+        // generally, these should be an integer formatted like 'cost="10"'.
+        //
+        // These attributes are used to reduce the amount of 'magic numbers'
+        // in the code. That is, numbers that are simply defined and have no
+        // affiliation with the rest of the code. Such numbers should not be defined
+        // in the code, but rather in configurations or HTML documents.
+        const upgradeCost = parseInt(element.getAttribute('cost'));
+        const upgradeClicks = parseInt(element.getAttribute('clicks'));
+
+        element.addEventListener((e) => {
+            if(clicks >= upgradeCost) {
+                clicks -= upgradeCost;
+                clickssec += upgradeClicks;
+
+                cpsLabel.innerHTML = clickssec;
+            }
+        })
+    })
     
     var intervalID = window.setInterval(myCallback, 1000);
     function myCallback() {
